3e1ff6798e0b54e24b39f8b3028fc39b8ab22bff
wushaobo <wushaobo.china@gmail.com>
2017-08-27 21:45:19 +0800

    move to docker hub from unstable daocloud

--- docker/prod/run.sh
+++ docker/prod/run.sh
@@ -10,5 +10,4 @@ app_dir=/opt/${cmd_name}
 log_dir=/var/log/${cmd_name}
 
 echo "Start listening ..."
-${app_dir}/${cmd_name}
-#${app_dir}/${cmd_name} >> ${log_dir}/stdout.log 2>> ${log_dir}/stderr.log
+${app_dir}/${cmd_name} >> ${log_dir}/stdout.log 2>> ${log_dir}/stderr.log
